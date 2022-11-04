2a6c0c6eb0b281f9e56875785ceb1b860f2aeef4
TienDat57 <dangtiendat1135@gmail.com>
2022-11-04 14:42:39 +0700

    [IDS - 01] | edit test request (crawl data)

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ headers = {
     'Accept': 'application/json, text/plain, */*',
     'Accept-Language': 'vi-VN,vi;q=0.8,en-US;q=0.5,en;q=0.3',
     'Referer': 'https://tiki.vn/?src=header_tiki',
-    'x-guest-token': '8jWSuIDBb2NGVzr6hsUZXpkP1FRin7lY',
+    'x-guest-token': 'xLOIgaKMRSBUwfeYkWJ6rThQbHdE7pDn',
     'Connection': 'keep-alive',
     'TE': 'Trailers',
 }
 
 params = {
-    'limit': '48',
+    'limit': '40',
     'include': 'sale-attrs,badges,product_links,brand,category,stock_item,advertisement',
     'aggregations': '1',
     'trackity_id': '70e316b0-96f2-dbe1-a2ed-43ff60419991',
@@ -24,15 +24,39 @@ params = {
     'urlKey':  'nha-cua-doi-song',
 }
 
-product_id = []
-for i in range(1, 18):
-    params['page'] = i
-    response = requests.get('https://tiki.vn/api/v2/products', headers=headers, params=params)#, cookies=cookies)
-    if response.status_code == 200:
-        print('request success!!!')
-        for record in response.json().get('data'):
-            product_id.append({'id': record.get('id')})
-    time.sleep(random.randrange(3, 10))
+def parse_products(product_json):
+    products = dict()
+    products['p_id'] = product_json.get('id')
+    products['p_shop_id'] = product_json.get('seller_product_id')
+    products['p_brand'] = product_json.get('brand_name')
+    products['p_name'] = product_json.get('name')
+    products['p_sold_quantity'] = product_json.get('quantity_sold').get('value') if product_json.get('quantity_sold') else 0
+    products['p_original_price'] = product_json.get('original_price')
+    products['p_discount_price'] = product_json.get('price')
+    products['p_discount_rate'] = product_json.get('discount_rate')
+    products['p_review_count'] = product_json.get('review_count')
+    products['p_rating_average'] = product_json.get('rating_average')
+    
+    return products
 
-df = pd.DataFrame(product_id)
-df.to_csv('product_id_ncds.csv', index=False)
+def get_all_pages():
+    print('Start crawling...')
+    all_products = []
+    for page in range(1, 50):
+        print('Crawling page: ', page)
+        params['page'] = page
+        response = requests.get('https://tiki.vn/api/v2/products', headers=headers, params=params)
+        if response.status_code == 200:
+            products_json = response.json().get('data')
+            for product in products_json:
+                all_products.append(parse_products(product))
+        else:
+            break
+        time.sleep(random.randint(1, 3))
+    print('Crawling completed!')
+    return all_products
+
+result = get_all_pages()
+    
+df = pd.DataFrame(result)
+df.to_csv('tiki.csv', index=False)
