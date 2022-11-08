32d018d2de653a3e6ac939e476a82ebf8df64cd4
TienDat57 <dangtiendat1135@gmail.com>
2022-11-09 00:43:54 +0700

    [IDS - 05] | Crawl p_review_count, p_rating_average

--- src/data/crawl_data/crawl_products_id.py
+++ src/data/crawl_data/crawl_products_id.py
@@ -16,33 +16,37 @@ headers = {
 params = {
    'limit': '40',
    'include': 'sale-attrs,badges,product_links,brand,category,stock_item,advertisement',
-   'aggregations': '2',
-   'trackity_id': 'b8bf2891-3eb9-7aa0-8dd1-a1aaf584cc05',
-   'category': '4221',
+   'aggregations': '1',
+   'trackity_id': '70e316b0-96f2-dbe1-a2ed-43ff60419991',
+   'category': '1883',
    'page': '1',
-   'src': 'c4221',
-   'urlKey': 'dien-tu-dien-lanh',
+   'src': 'c1883',
+   'urlKey':  'nha-cua-doi-song',
 }
 
+def parse_products(product_json):
+   products = dict()
+   products['p_id'] = product_json.get('id')
+   products['p_review_count'] = product_json.get('review_count')
+   products['p_rating_average'] = product_json.get('rating_average')
+   return products
+
 def get_product_id_all_pages():
-   url_tiki = 'https://tiki.vn/api/v2/products'
-   max_page = 50
-   products_id = []
-   
-   print('Start crawling...')   
-   for i in range(1, max_page):
-      params['page'] = i
-      response = requests.get(url_tiki, headers=headers, params=params)
+   all_products = []
+   for page in range(1, 50):
+      print('Crawling page: ', page)
+      params['page'] = page
+      response = requests.get('https://tiki.vn/api/v2/products', headers=headers, params=params)
       if response.status_code == 200:
-         for record in response.json().get('data'):
-            products_id.append({'id': record.get('id')})
+         products_json = response.json().get('data')
+         for product in products_json:
+               all_products.append(parse_products(product))
       else:
          break
       time.sleep(random.randint(1, 3))
-   print('Crawling completed!')
-   return products_id
+   return all_products
 
-list_products_id = get_product_id_all_pages()
-
-df = pd.DataFrame(list_products_id)
+result = get_product_id_all_pages()
+    
+df = pd.DataFrame(result)
 df.to_csv('../../../data/raw/products_id.csv', index=False)
