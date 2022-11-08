1ab771cb04b8b99718b9bd106b1554d4c2e266c8
nthang2505 <tn864440@gmail.com>
2022-11-08 23:31:50 +0700

    [IDS - 03] | Crawl data product detail successful

--- src/data/crawl_data/helpers.py
+++ src/data/crawl_data/helpers.py
@@ -3,18 +3,18 @@ headers = {
    'Accept': 'application/json, text/plain, */*',
    'Accept-Language': 'vi-VN,vi;q=0.8,en-US;q=0.5,en;q=0.3',
    'Referer': 'https://tiki.vn/?src=header_tiki',
-   'x-guest-token': '8jWSuIDBb2NGVzr6hsUZXpkP1FRin7lY',
+   'x-guest-token': 'xLOIgaKMRSBUwfeYkWJ6rThQbHdE7pDn',
    'Connection': 'keep-alive',
    'TE': 'Trailers',
 }
 
 params = {
-   'limit': '48',
+   'limit': '40',
    'include': 'sale-attrs,badges,product_links,brand,category,stock_item,advertisement',
-   'aggregations': '1',
-   'trackity_id': '70e316b0-96f2-dbe1-a2ed-43ff60419991',
-   'category': '1883',
+   'aggregations': '2',
+   'trackity_id': 'b8bf2891-3eb9-7aa0-8dd1-a1aaf584cc05',
+   'category': '4221',
    'page': '1',
-   'src': 'c1883',
-   'urlKey':  'nha-cua-doi-song',
+   'src': 'c4221',
+   'urlKey': 'dien-tu-dien-lanh',
 }
